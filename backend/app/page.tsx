89b745b82075a0ffaa3a5516231ed286a9b733b1
ashutosh <ashutoshsahni833@gmail.com>
2024-12-27 00:20:19 +0530

    backend

---
+++ backend/app/page.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const getUserData = async () => {
+  await new Promise((r)=>{setTimeout(r,2000)})
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
+  return response.data;
+};
+
+export default async function home(params: any) {
+  const userDetails = await getUserData();
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex justify-center"> 
+          <div className="border p-8 rounded">
+            <div> {userDetails.name} </div>
+            <div> {userDetails.email} </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
